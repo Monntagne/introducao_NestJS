eba255f37e01ca8fdf8e604f6ca663adde7b9faa
VINICIUS CARLOS DA SILVA SANTOS <vinicius.cssantos3@senacsp.edu.br>
2026-06-03 22:14:07 -0300

    corrigindo erros de um completo imbecil

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProdutosModule } from './produtos/produtos.module';
-import { GService } from './controller/g/g.service';
-import { ControllerService } from './controller/controller.service';
+
 
 @Module({
   imports: [ProdutosModule],
   controllers: [AppController],
-  providers: [AppService, GService, ControllerService],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/produtos/produtos.module.ts
+++ src/produtos/produtos.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { GService } from './g/g.service';
-import { ControllerService } from './controller/controller.service';
 import { ProdutosService } from './produtos.service';
 import { ProdutosController } from './produtos.controller';
 
 @Module({
-  providers: [GService, ControllerService, ProdutosService],
+  providers: [ ProdutosService],
   controllers: [ProdutosController]
 })
 export class ProdutosModule {}
